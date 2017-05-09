82b194a18eda5413c002359c035599c444b5d521
Zak <zak@kindlingsocial.com>
2017-05-09 10:26:26 -0700

    cleaned up the api call

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -5,12 +5,10 @@ const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?app
 module.exports = {
   getTemp(location) {
     const encodedLocation = encodeURIComponent(location); // proper format for http
-    console.log('Encoded Location:', encodedLocation);
     let reqUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(reqUrl) // API Call
       .then((res) => {
-        debugger;
         if (res.data.cod !== 200) {
           throw new Error(res.data.message);
         } else {
